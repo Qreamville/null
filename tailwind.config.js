9923190f8dca6cb9a23eb12a8c533be99ff021cf
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-26 02:14:48 -1200

    Upadate package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "320px",
+      sm: "576px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1200px",
+    },
+    extend: {
+      colors: {},
+    },
   },
   plugins: [],
 };
