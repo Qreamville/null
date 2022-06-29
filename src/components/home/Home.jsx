7dee320bfd3cc38f67e0602b2d0c1a349489acbe
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-29 02:39:03 -1200

    Add pacman loader

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Loader from "react-loaders";
 import pdf from "../../assets/Resume.pdf";
 import "./home.css";
 
 const Home = () => {
   return (
-    <div className="home section-container">
-      <div className="home-text__zone">
-        <h1 className="text-white text-5xl">
-          Hi, 👋 <br /> I'm Balikis Oyeleye
-        </h1>
-        <h2>Frontend Developer / ReactJs Developer</h2>
-        <div className="flex flex-row gap-6">
-          <Link to="/contact" className="home-contact">
-            Contact Me
-          </Link>
-          <a className="home-download" href={pdf} download>
-            Download CV
-          </a>
+    <>
+      <div className="home section-container">
+        <div className="home-text__zone">
+          <h1 className="text-white text-5xl">
+            Hi, 👋 <br /> I'm Balikis Oyeleye
+          </h1>
+          <h2>Frontend Developer / ReactJs Developer</h2>
+          <div className="flex flex-row gap-6">
+            <Link to="/contact" className="home-contact">
+              Contact Me
+            </Link>
+            <a className="home-download" href={pdf} download>
+              Download CV
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
