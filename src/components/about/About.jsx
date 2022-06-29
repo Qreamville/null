729a49d53446a439253a8e9c46b5aeba89668e1a
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-29 00:50:03 -1200

    Update about.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./about.css";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about section-container">
+      <div className="about-text__zone">
+        <h1>ABOUT ME</h1>
+        <p>
+          Balikis Oyeleye (Pronounced as Ba-Li-Kiss) is a Civil Engineer
+          transitioning into web development. She is a self-taught Frontend
+          Developer specializing in building web application and responsive
+          websites.
+        </p>
+        <p>
+          She is an optimistic, enthusiastic and motivated Junior Frontend
+          Developer that is seeking an opportunity to contribute to Institutions
+          by implementing ideas, concepts and methodologies learned thereby
+          adding values to the organization.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
