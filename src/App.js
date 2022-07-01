549743ede0762244f1e3a3e153f6260d9b783c8e
Qreamville <Oyeleyebalikis@yahoo.com>
2022-06-30 23:36:08 -1200

    Create contact components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { Layout, Home, About } from "./components";
+import { Layout, Home, About, Contact } from "./components";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
