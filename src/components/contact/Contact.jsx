ac59a3bd9fd30cfed8a8b5807c4335ca8bc23252
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-01 23:37:28 -1200

    Update contact.js

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useRef } from "react";
 import Loader from "react-loaders";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_KEY,
+        process.env.REACT_APP_TEMPLATE_KEY,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          alert(`Messege sent ${result.text}`);
+          window.location.reload(false);
+        },
+        () => {
+          alert("Fail to send message, Please try again");
+        }
+      );
+  };
+
   return (
     <>
       <div className="section-container contact">
         <div className="contact-text__zone">
           <h1>Contact Me</h1>
           <div className="contact-form">
-            <form className="pt-4">
+            <form className="pt-4" ref={form} onSubmit={sendEmail}>
               <ul className="flex flex-col gap-6">
                 <div className="flex gap-4">
                   <li className="w-1/2">
                     <input
                       type="text"
-                      name="name"
+                      name="from_name"
                       placeholder="Your Name"
                       className="w-full"
                       required
