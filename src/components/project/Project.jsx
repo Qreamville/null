7027095c753d0aa7f2d937a34fc9a75df876c028
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-04 00:18:30 -1200

    Update project.jsx

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import Loader from "react-loaders";
 import "./project.css";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <>
+      <div className="projects section-container">
+        <div className="project-text__zone">
+          <h1>Projects</h1>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
 };
 
 export default Project;
