2a707f8cd7ae843d533b287b34885fd8fd595a19
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-03 23:35:16 -1200

    Add files via text

---
+++ src/assets/projectData.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    id: 1,
+    name: "Sneakers",
+    about: "An e-Commerce website where you can shop for sneakers, scandal and heels. It also provide features such as cart management, search function etc",
+    stack: ["ReactJs", "React-Bootstrap", "Bootstrap"],
+    links: [
+      { name: "github", href: "https://github.com/Qreamville/E-commerce-Sneakers-Website" },
+      { name: "live-link", href: "https://qreamville.github.io/E-commerce-Sneakers-Website/" }
+    ]
+
+  },
+  {
+    id: 2,
+    name: "Mathwhiz",
+    about: "A math quiz that test your multiplication, division, addition and subtraction skills in 30 seconds. It also includes dark/light theme",
+    stack: ["ReactJs", "TailwindCss", "React-Icons"],
+    links: [
+      { name: "github", href: "https://github.com/Qreamville/Mathwhiz" },
+      { name: "live-link", href: "https://qreamville.github.io/Mathwhiz" }
+    ]
+
+  },
+  {
+    id: 3,
+    name: "Currency-Converter",
+    about: "It is an application that fetches the current exchange rate of different currencies with the help of exchange rates api.",
+    stack: ["Api", "ReactJs", "bootstrap", "axios"],
+    links: [
+      { name: "github", href: "https://github.com/Qreamville/Currency-Converter" },
+      { name: "live-link", href: "https://qreamville.github.io/Currency-Converter/" }
+    ]
+
+  },
+  {
+    id: 4,
+    name: "Rock- Paper - Scissors - Lizard - Spock",
+    about: "Rock Paper Scissors Lizard Spock is an extension of the classic game of chance, Rock Paper Scissors, created by Sam Kass and Karen Bryla. This application also store users score in local storage",
+    stack: ["TailwindCss", "ReactJs",],
+    links: [
+      { name: "github", href: "https://github.com/Qreamville/Rock-Paper-Scissors-Lizard-Spock" },
+      { name: "live-link", href: "https://qreamville.github.io/Rock-Paper-Scissors-Lizard-Spock/" }
+    ]
+
+  },
+]
