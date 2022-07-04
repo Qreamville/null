ea8941536bde000d9c12457e3e7e3a7ad44df2b6
Qreamville <Oyeleyebalikis@yahoo.com>
2022-07-04 00:02:15 -1200

    Add images

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -1,7 +1,13 @@
+import sneakers from "./images/sneakers-website.png"
+import mathwhiz from "./images/mathwhiz-website.png"
+import currency from "currency-website"
+import rock from "rock-website"
+
 export const data = [
   {
     id: 1,
     name: "Sneakers",
+    image: sneakers,
     about: "An e-Commerce website where you can shop for sneakers, scandal and heels. It also provide features such as cart management, search function etc",
     stack: ["ReactJs", "React-Bootstrap", "Bootstrap"],
     links: [
@@ -13,6 +19,7 @@ export const data = [
   {
     id: 2,
     name: "Mathwhiz",
+    image: mathwhiz,
     about: "A math quiz that test your multiplication, division, addition and subtraction skills in 30 seconds. It also includes dark/light theme",
     stack: ["ReactJs", "TailwindCss", "React-Icons"],
     links: [
@@ -24,6 +31,7 @@ export const data = [
   {
     id: 3,
     name: "Currency-Converter",
+    image: currency,
     about: "It is an application that fetches the current exchange rate of different currencies with the help of exchange rates api.",
     stack: ["Api", "ReactJs", "bootstrap", "axios"],
     links: [
@@ -35,6 +43,7 @@ export const data = [
   {
     id: 4,
     name: "Rock- Paper - Scissors - Lizard - Spock",
+    image: rock,
     about: "Rock Paper Scissors Lizard Spock is an extension of the classic game of chance, Rock Paper Scissors, created by Sam Kass and Karen Bryla. This application also store users score in local storage",
     stack: ["TailwindCss", "ReactJs",],
     links: [
